72971b5675fc04565940b1b153a82ddd7c4e8b83
yazelin <yazelin@ching-tech.com>
2026-01-23 16:13:56 +0800

    fix: change base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
     return {
       // 改用相對路徑，增加部署靈活性
-      base: './',
+      base: '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
